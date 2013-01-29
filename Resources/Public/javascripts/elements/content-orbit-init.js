a1e9d89fdd1013eb6d3ff20dee84f8ce0c42df9a
Beda Steinacher <steinacher.beda@bluewin.ch>
2013-01-29 22:39:15 +0100

    [FEATURE] Use directional Navigation

--- Resources/Public/javascripts/elements/content-orbit-init.js
+++ Resources/Public/javascripts/elements/content-orbit-init.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         pauseOnHover:false, // if you hover pauses the slider
         startClockOnMouseOut:false, // if clock should start on MouseOut
         startClockOnMouseOutAfter:1000, // how long after MouseOut should the timer start again
-        directionalNav:true, // manual advancing directional navs
+        directionalNav:{directionalNav}, // manual advancing directional navs
         captions:true, // do you want captions?
         captionAnimation:'fade', // fade, slideOpen, none
         captionAnimationSpeed:800, // if so how quickly should they animate in
