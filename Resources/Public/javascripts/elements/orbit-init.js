52af4908bfc752e23aff192455c2646fede472d6
Beda Steinacher <beda.steinacher@creaocchio.ch>
2013-02-04 09:16:04 +0100

    [FEATURE] Add option to activate timer

--- Resources/Public/javascripts/elements/orbit-init.js
+++ Resources/Public/javascripts/elements/orbit-init.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#{id}').orbit({
         animation:'fade', // fade, horizontal-slide, vertical-slide, horizontal-push
         animationSpeed:800, // how fast animtions are
-        timer:true, // true or false to have the timer
+        timer:{timer}, // true or false to have the timer
         resetTimerOnClick:false, // true resets the timer instead of pausing slideshow progress
         advanceSpeed:4000, // if timer is enabled, time between transitions
         pauseOnHover:false, // if you hover pauses the slider
